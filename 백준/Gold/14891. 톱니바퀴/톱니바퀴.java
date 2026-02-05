d7611a18e364094282fbca85c2a4c46395b4d175
DongJun Choi <qkqehenr37@gmail.com>
2026-02-05 22:02:34 +0900

    [Gold V] Title: 톱니바퀴, Time: 64 ms, Memory: 11608 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
@@ -5,6 +5,9 @@ public class Main {
 	
 	static int[][] wheel = new int[4][8];
 	static int[] pos = new int[4];
+	static int K;
+	static int n;
+	static int dir;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,13 +17,14 @@ public class Main {
 				wheel[i][j] = nums[j] - '0';
 			}
 		}
-		int k = Integer.parseInt(br.readLine());
-		for (int i = 0; i < k; i++) {
+		K = Integer.parseInt(br.readLine());
+		for (int i = 0; i < K; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int n = Integer.parseInt(st.nextToken());
-			int dir = Integer.parseInt(st.nextToken()); // 1: clockwise, 2: counterClockwise
+			n = Integer.parseInt(st.nextToken());
+			dir = Integer.parseInt(st.nextToken()); // 1: clockwise, 2: counterClockwise
 			rotate(n, dir, 0);
 		}
+		
 		int total = 0;
 		for (int i = 0; i < 4; i++) {
 			total += wheel[i][(8-pos[i])%8] * Math.pow(2, i);
@@ -29,52 +33,18 @@ public class Main {
 		
 
 	}
-
+	
+	
 	static void rotate(int n, int dir, int prev) {
-		
 		int prevPos = pos[n-1];
-		pos[n-1] += dir;
-		pos[n-1] += 8;
-		pos[n-1] %= 8;
+		pos[n-1] = (pos[n-1] + dir + 8)%8;
 		
-		if (n == 1) {
-			if (prev != 2) {
-				if (wheel[n-1][(8-prevPos+2)%8] != wheel[n][(8-pos[n]+6)%8]) {
-					rotate(n+1, dir*(-1), n);
-				}
-			}
-			
-		} else if (n == 2) {
-			if (prev != 1) {
-				if (wheel[n-1][(8-prevPos+6)%8] != wheel[n-2][(8-pos[n-2]+2)%8]) {
-					rotate(n-1, dir*(-1), n);
-				}
-			}
-			if (prev != 3) {
-				if (wheel[n-1][(8-prevPos+2)%8] != wheel[n][(8-pos[n]+6)%8]) {
-					rotate(n+1, dir*(-1), n);
-				}
-			}
-			
-		} else if (n == 3) {
-			if (prev != 2) {
-				if (wheel[n-1][(8-prevPos+6)%8] != wheel[n-2][(8-pos[n-2]+2)%8]) {
-					rotate(n-1, dir*(-1), n);
-				}
-			}
-			if (prev != 4) {
-				if (wheel[n-1][(8-prevPos+2)%8] != wheel[n][(8-pos[n]+6)%8]) {
-					rotate(n+1, dir*(-1), n);
-				}
-			}
-			
-		} else if (n == 4) {
-			if (prev != 3) {
-				if (wheel[n-1][(8-prevPos+6)%8] != wheel[n-2][(8-pos[n-2]+2)%8]) {
-					rotate(n-1, dir*(-1), n);
-				}
-			}
+		if (n > 1 && prev != n-1 && wheel[n-1][(8-prevPos+6)%8] != wheel[n-2][(8-pos[n-2]+2)%8]) {
+			rotate(n-1, dir*(-1), n);
 		}
 		
+		if (n < 4 && prev != n+1 && wheel[n-1][(8-prevPos+2)%8] != wheel[n][(8-pos[n]+6)%8]) {
+			rotate(n+1, dir*(-1), n);
+		}
 	}
 }
