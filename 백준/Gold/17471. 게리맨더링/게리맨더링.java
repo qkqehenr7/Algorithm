c388bf8a488bb08fa30dcc1fe961227413c7a32c
DongJun Choi <qkqehenr37@gmail.com>
2026-02-06 10:03:21 +0900

    [Gold III] Title: 게리맨더링, Time: 80 ms, Memory: 12824 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -12,17 +12,14 @@ public class Main {
 	static int N;
 	static int[] population;
 	static List<List<Integer>> adj = new ArrayList<>();
-	static List<List<Integer>> subset = new ArrayList<>();
-	static boolean[] visited;
 	static int totalPop = 0;
-	static int min_pop = Integer.MAX_VALUE;
-	static int cnt;
+	static int min_diff = Integer.MAX_VALUE;
+	static int FULL;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		population = new int[N];
-		visited = new boolean[N];
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
@@ -42,83 +39,62 @@ public class Main {
 			adj.add(nb);
 		}
 		
-		subset(0);
+		FULL = (1 << N) - 1;
+
+		// 2. 서브셋 구하기
+		for (int mask = 1; mask < (1 << N) / 2; mask++) {
+
+		    int comp = FULL ^ mask;
+
+		    if (!isConnected(mask) || !isConnected(comp)) continue;
+
+		    int sum = 0;
+		    for (int i = 0; i < N; i++) {
+		        if ((mask & (1 << i)) != 0) {
+		            sum += population[i];
+		        }
+		    }
+
+		    int diff = Math.abs(totalPop - 2 * sum);
+		    min_diff = min_diff < diff ? min_diff : diff;
+		}
 		
-		// 2-3. 공집합을 제외한 진부분집합 만들기
-		for (List<Integer> elements : subset) {
-			// 2-4. 여집합 만들기
-			List<Integer> comp = new ArrayList<>();
-			for (int i = 1; i <= N; i++) {
-				if (!elements.contains(i)) {
-					comp.add(i);
-				}
-			}
-			
-			// 4-1. 두 선거구가 모두 연결된 상태인 경우 최솟값 갱신하기
-			if (isConnected(elements) && isConnected(comp)) {
-				int total = 0;
-				for (int element : elements) {
-					total += population[element-1];
-				}
-				int diff = Math.abs((totalPop - 2*total));
-				min_pop = min_pop > diff ? diff : min_pop;
-			}
-		}
-		System.out.println((min_pop == Integer.MAX_VALUE) ? -1 : min_pop);
+		System.out.println((min_diff == Integer.MAX_VALUE) ? -1 : min_diff);
 	}
 	
-	// 2-1. 부분집합 만들기
-	static void subset(int depth) {
-		if (depth == N && check()) {
-			return;
-		}
-		if (depth == N) {
-			List<Integer> elements = new ArrayList<>();
-			for (int i = 0; i < N; i++) {
-				if (visited[i]) elements.add(i+1);
-			}
-			subset.add(elements);
-			return;
-		}
-		visited[depth] = true;
-		subset(depth+1);
-		visited[depth] = false;
-		subset(depth+1);	
-	}
-	
-	// 2-2. 모든 부분집합을 만들지 않도록 하기
-	static boolean check() {
-		int cnt = 0;
-		for (int i = 0; i < N; i++) {
-			if (visited[i]) cnt++; 
-		}
-		return cnt > N/2 || cnt == 0;
-	}
-	
-	// 4-2. 같은 선거구 내 노드들의 상호연결 여부 확인하기
-	static boolean isConnected(List<Integer> elements) {
+	// 3. 같은 선거구 내 노드들의 상호연결 여부 확인하기
+	static boolean isConnected(int mask) {
 		boolean[] vis = new boolean[N];
 		Queue<Integer> queue = new ArrayDeque<>();
-		queue.offer(elements.get(0));
-		vis[elements.get(0)-1] = true;
+		
+		// 시작 노드 찾기
+	    int start = -1;
+	    for (int i = 0; i < N; i++) {
+	        if ((mask & (1 << i)) != 0) {
+	            start = i;
+	            break;
+	        }
+	    }
+	    
+	    if (start == -1) return false;
+	    
+	    queue.offer(start);
+	    vis[start] = true;
+	    int cnt = 1;
 		
 		while (!queue.isEmpty()) {
 			int curr = queue.poll();
 			
-			for (int node : adj.get(curr-1)) {
+			for (int next : adj.get(curr)) {
+				next--;
 				// 방문하지 않았으면서 subset에 포함된 노드만 큐에 넣기
-				if (!vis[node-1] && elements.contains(node)) {
-					vis[node-1] = true;
-					queue.offer(node);
+				if (!vis[next] && (mask & (1 << next)) != 0)  {
+					vis[next] = true;
+					queue.offer(next);
+					cnt++;
 				}
 			}
 		}
-		
-		for (int node : elements) {
-			if(!vis[node-1]) {
-				return false;
-			}
-		}
-		return true;
+		return cnt == Integer.bitCount(mask);
 	}
 }
