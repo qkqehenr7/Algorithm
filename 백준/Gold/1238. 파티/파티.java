435ccc62e3bc70332553bb14afa2b7134e156f83
DongJun Choi <qkqehenr37@gmail.com>
2026-03-12 22:17:16 +0900

    [Gold III] Title: 파티, Time: 136 ms, Memory: 16560 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -5,14 +5,12 @@ public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static int[][] dist;
     static int N, M, X;
-    static int T, from, to;
     static int max_value = 1_000_000;
-    static List<List<Edge>> adj;
+    static List<List<Edge>> adj, reverseAdj;
 
     static class Edge implements Comparable<Edge> {
-        int to;
+    	int to;
         int weight;
 
         Edge(int to, int weight) {
@@ -28,35 +26,40 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         init();
-        for (int i = 1; i <= N; i++) {
-            dijkstra(i);
-        }
-
+        int[] distHomeToX = dijkstra(adj);
+        int[] distXToHome = dijkstra(reverseAdj);
+        
         int maxTime = 0;
         for (int i = 1; i <= N; i++) {
-            maxTime = Math.max(maxTime, dist[i][X] + dist[X][i]);
+            maxTime = Math.max(maxTime, distHomeToX[i] + distXToHome[i]);
         }
         System.out.println(maxTime);
     }
 
-    static void dijkstra(int village) {
+    static int[] dijkstra(List<List<Edge>> adjList) {
         PriorityQueue<Edge> pq = new PriorityQueue<>();
-        pq.offer(new Edge(village, 0));
+        pq.offer(new Edge(X, 0));
+        
+        int[] dist = new int[N+1];
+        Arrays.fill(dist, max_value);
+        dist[X] = 0;
 
         while (!pq.isEmpty()) {
             Edge curr = pq.poll();
             int to = curr.to;
             int weight = curr.weight;
 
-            if (dist[village][to] < weight) continue;
+            if (dist[to] < weight) continue;
 
-            for (Edge next : adj.get(to)) {
-                if (dist[village][next.to] > weight + next.weight) {
-                    dist[village][next.to] = weight + next.weight;
-                    pq.offer(new Edge(next.to, dist[village][next.to]));
+            for (Edge next : adjList.get(to)) {
+                if (dist[next.to] > weight + next.weight) {
+                    dist[next.to] = weight + next.weight;
+                    pq.offer(new Edge(next.to, dist[next.to]));
                 }
             }
         }
+        
+        return dist;
     }
 
     static void init() throws IOException {
@@ -66,23 +69,21 @@ public class Main {
         X = Integer.parseInt(st.nextToken());
 
         adj = new ArrayList<>();
+        reverseAdj = new ArrayList<>();
         for (int i = 0; i <= N; i++) {
             adj.add(new ArrayList<>());
+            reverseAdj.add(new ArrayList<>());
         }
-
-        dist = new int[N+1][N+1];
-        for (int i = 0; i <= N; i++) {
-            Arrays.fill(dist[i], max_value);
-            dist[i][i] = 0;
-        }
+        
 
         for (int i = 1; i <= M; i++) {
             st = new StringTokenizer(br.readLine());
-            from = Integer.parseInt(st.nextToken());
-            to = Integer.parseInt(st.nextToken());
-            T = Integer.parseInt(st.nextToken());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int T = Integer.parseInt(st.nextToken());
 
             adj.get(from).add(new Edge(to, T));
+            reverseAdj.get(to).add(new Edge(from, T));
         }
     }
 }
